84ddb7cf7c9ed98c091dc24159337bfeed9edeb5
sarahmediciner <sarahsantana120@hotmail.com>
2024-12-04 07:49:48 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,24 @@
+// Gabriel Berto Tavares de Brito 3°D  27/11/2024
+
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é Hi (RAI)'
+)
